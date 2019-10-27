5c2eb9a57c7223d43b132c25b3acb4ecc6212d0b
Ellysia Li <li6201@pacificu.edu>
2019-10-26 19:06:45 -0700

    Fixed airport Makefile's clean, added structures and function prototypes to list.h and added skeleton code in airplane.c

--- Airport/include/airport.h
+++ Airport/include/airport.h
@@ -12,3 +12,162 @@
 #include "../../GenericDynamicList/include/list.h"
 #include "../../GenericDynamicPriorityQ/include/pqueue.h"
 #include "../../GenericDynamicQ/include/queue.h"
+
+//*************************************************************************
+// Constants
+//*************************************************************************
+#define MAX_ERROR_AIRPORT_CHARS 64
+#define NUMBER_OF_RUNWAYS 3
+
+enum {NO_AIRPORT_ERROR = 0,
+			ERROR_NO_AIRPORT_CREATE,
+			ERROR_NO_AIRPORT_TERMINATE,
+			ERROR_INVALID_AIRPORT,
+			ERROR_FULL_AIRPORT,
+			ERROR_NULL_AIRPORT_PTR,
+			ERROR_EMPTY_AIRPORT};
+
+#define NUMBER_OF_AIRPORT_ERRORS ERROR_EMPTY_AIRPORT - NO_AIRPORT_ERROR + 1
+
+//*************************************************************************
+// Error Messages
+//*************************************************************************
+#define LOAD_AIRPORT_ERRORS strcpy(gszAIRPORTErrors[NO_AIRPORT_ERROR], \
+		"No Error.");\
+strcpy(gszAIRPORTErrors[ERROR_NO_AIRPORT_CREATE], \
+		"Error: No Airport Create.");\
+strcpy(gszAIRPORTErrors[ERROR_NO_AIRPORT_TERMINATE], \
+		"Error: No Airport Terminate.");\
+strcpy(gszAIRPORTErrors[ERROR_INVALID_AIRPORT], "Error: Invalid Airport.");\
+strcpy(gszAIRPORTErrors[ERROR_FULL_AIRPORT], "Error: Full Airport.");\
+strcpy(gszAIRPORTErrors[ERROR_NULL_AIRPORT_PTR], "Error: NULL Pointer.");\
+strcpy(gszAIRPORTErrors[ERROR_EMPTY_AIRPORT], "Error: Empty Airport.");
+
+//*************************************************************************
+// User-defined types
+//*************************************************************************
+typedef enum Runway {EMPTY = 0, TAKEOFF, LANDING, EMERGENCY} Runway;
+
+typedef struct AirportStats
+{
+		int totalTakeoffTime, totalLandingTime, totalFlyingTimeRemaining,
+		    numTakeoffs, numLandings, numEmergencyLandings, numCrashes;
+} AirportStats;
+
+typedef struct Airport
+{
+	Runway ezRunways [NUMBER_OF_RUNWAYS];
+//	int firstOpenRunway;
+	PriorityQueue sLandingQueue;
+	Queue sTakeoffQueue;
+} Airport;
+
+typedef Airport* AirportPtr;
+
+/**************************************************************************
+*										Allocation and Deallocation
+**************************************************************************/
+extern void airportCreate (AirportPtr psAirport);
+// results: If airport can be created, then airport exists and is empty
+//					otherwise, ERROR_NO_AIRPORT_CREATE
+
+
+extern void airportTerminate (AirportPtr psAirport);
+// results: If Q can be terminated, then Q no longer exists and is empty
+//				   otherwise, ERROR_NO_AIRPORT_TERMINATE
+
+extern void airportLoadErrorMessages ();
+// results:	Loads the error message strings for the error handler to use
+//					No error conditions
+
+/**************************************************************************
+*							Checking number of elements in Airport's queues
+**************************************************************************/
+extern int airportLandingQSize (const AirportPtr psAirport);
+// results: Returns the number of elements in the LandingQ
+// 					error code priority: ERROR_INVALID_AIRPORT if AIRPORT is NULL
+
+extern int airportTakeoffQSize (const AirportPtr psAirport);
+// results: Returns the number of elements in the TakeoffQ
+// 					error code priority: ERROR_INVALID_AIRPORT if AIRPORT is NULL
+
+extern bool airportQsAreEmpty (const AirportPtr psAirport);
+// results: If both Q's are empty, return true; otherwise, return false
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+/**************************************************************************
+*										Inserting, Deleting, and Updating
+**************************************************************************/
+extern void airportAddLandingPlane (AirportPtr psAirport, const int *pGas);
+// results: Insert a plane to the landing queue.
+//					error code priority: ERROR_INVALID_AIRPORT, ERROR_NULL_AIRPORT_PTR
+
+extern void airportAddTakeoffPlane (AirportPtr psAirport);
+// results: Insert a plane to the takeoff queue.
+//					error code priority: ERROR_INVALID_AIRPORT
+
+extern void airportLandPlane (AirportPtr psAirport, int *pTime, int *pGas);
+// results: Remove a plane from the landing queue.
+//					error code priority: ERROR_INVALID_AIRPORT,
+//															 ERROR_NULL_AIRPORT_PTR, ERROR_EMPTY_AIRPORT
+
+extern void airportTakeoffPlane (AirportPtr psAirport, int *pTime);
+// results: Remove a plane from the landing queue.
+//					error code priority: ERROR_INVALID_AIRPORT,
+//															 ERROR_NULL_AIRPORT_PTR, ERROR_EMPTY_AIRPORT
+
+extern void airportCrashPlane (AirportPtr psAirport, int *pTime);
+// results: Crash a plane from the landing queue.
+//					error code priority: ERROR_INVALID_AIRPORT, ERROR_EMPTY_AIRPORT
+
+extern void airportAssignRunways (AirportPtr psAirport);
+// results: Assign the appropriate planes to runways (if necessary)
+//					error code priority: ERROR_INVALID_AIRPORT
+
+extern void airportIncrementClock (AirportPtr psAirport);
+// results: Increment/decrement the time/gas of each airplane by one.
+//					error code priority: ERROR_INVALID_AIRPORT
+
+/**************************************************************************
+*													Get/Check/Peek Operations
+**************************************************************************/
+extern char airportGetRunwayStatus (const AirportPtr psAirport,
+																		const int number);
+// results: Returns the status of the (number)th runway
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+extern bool airportHasEmergency (const AirportPtr psAirport);
+// requires: psQueue is not empty
+// results: If the landing queue has a priority of 0, return true; otherwise,
+//          return false
+// 					error code priority: ERROR_INVALID_AIRPORT, ERROR_EMPTY_AIRPORT
+
+extern bool airportRunwaysAreOpen (const AirportPtr psAirport);
+// results: If at least one runway is empty, return true; otherwise, return
+//          false
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+extern bool airportRunwaysAreOpen (const AirportPtr psAirport);
+// results: If at least one runway is empty, return true; otherwise, return
+//          false
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+extern float getAverageLandingTime (const AirportPtr psAirport);
+// results: Return the average landing waiting time
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+extern float getAverageTakeoffTime (const AirportPtr psAirport);
+// results: Return the average takeoff waiting time
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+extern float getAverageFlyingTimeRemaining (const AirportPtr psAirport);
+// results: Return the average flying time remaining
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+extern float getNumEmergencyLandings (const AirportPtr psAirport);
+// results: Return the number of emergency landings
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+extern float getNumCrashes (const AirportPtr psAirport);
+// results: Return the number of crashes
+// 					error code priority: ERROR_INVALID_AIRPORT
--- Airport/src/airport.c
+++ Airport/src/airport.c
@@ -7,6 +7,7 @@
  Purpose:    Implementation of an Airport Simulator
  *************************************************************************/
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 
@@ -14,3 +15,156 @@
 #include "../../GenericDynamicList/include/list.h"
 #include "../../GenericDynamicPriorityQ/include/pqueue.h"
 #include "../../GenericDynamicQ/include/queue.h"
+
+void airportCreate (AirportPtr psAirport)
+{
+
+}
+// results: If airport can be created, then airport exists and is empty
+//					otherwise, ERROR_NO_AIRPORT_CREATE
+
+
+void airportTerminate (AirportPtr psAirport)
+{
+
+}
+// results: If Q can be terminated, then Q no longer exists and is empty
+//				   otherwise, ERROR_NO_AIRPORT_TERMINATE
+
+void airportLoadErrorMessages ()
+{
+
+}
+// results:	Loads the error message strings for the error handler to use
+//					No error conditions
+
+int airportLandQSize (const AirportPtr psAirport)
+{
+	return 0;
+}
+// results: Returns the number of elements in the LandQ
+// 					error code priority: ERROR_INVALID_AIRPORT if AIRPORT is NULL
+
+int airportTakeoffQSize (const AirportPtr psAirport)
+{
+	return 0;
+}
+// results: Returns the number of elements in the TakeoffQ
+// 					error code priority: ERROR_INVALID_AIRPORT if AIRPORT is NULL
+
+bool airportQsAreEmpty (const AirportPtr psAirport)
+{
+	return true;
+}
+// results: If both Q's are empty, return true; otherwise, return false
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+void airportAddLandPlane (AirportPtr psAirport, const int *pGas)
+{
+
+}
+// results: Insert a plane to the land queue.
+//					error code priority: ERROR_INVALID_AIRPORT, ERROR_NULL_AIRPORT_PTR
+
+void airportAddTakeoffPlane (AirportPtr psAirport)
+{
+
+}
+// results: Insert a plane to the takeoff queue.
+//					error code priority: ERROR_INVALID_AIRPORT
+
+void airportLandPlane (AirportPtr psAirport, int *pTime, int *pGas)
+{
+
+}
+// results: Remove a plane from the land queue.
+//					error code priority: ERROR_INVALID_AIRPORT,
+//															 ERROR_NULL_AIRPORT_PTR, ERROR_EMPTY_AIRPORT
+
+void airportTakeoffPlane (AirportPtr psAirport, int *pTime)
+{
+
+}
+// results: Remove a plane from the land queue.
+//					error code priority: ERROR_INVALID_AIRPORT,
+//															 ERROR_NULL_AIRPORT_PTR, ERROR_EMPTY_AIRPORT
+
+void airportCrashPlane (AirportPtr psAirport, int *pTime)
+{
+
+}
+// results: Crash a plane from the land queue.
+//					error code priority: ERROR_INVALID_AIRPORT, ERROR_EMPTY_AIRPORT
+
+void airportAssignRunways (AirportPtr psAirport)
+{
+
+}
+// results: Assign the appropriate planes to runways (if necessary)
+//					error code priority: ERROR_INVALID_AIRPORT
+
+void airportIncrementClock (AirportPtr psAirport)
+{
+
+}
+// results: Increment/decrement the time/gas of each airplane by one.
+//					error code priority: ERROR_INVALID_AIRPORT
+
+char airportGetRunwayStatus (const AirportPtr psAirport, const int number)
+{
+	return true;
+}
+// results: Returns the status of the (number)th runway
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+bool airportHasEmergency (const AirportPtr psAirport)
+{
+	return true;
+}
+// requires: psQueue is not empty
+// results: If the land queue has a priority of 0, return true; otherwise,
+//          return false
+// 					error code priority: ERROR_INVALID_AIRPORT, ERROR_EMPTY_AIRPORT
+
+bool airportRunwaysAreOpen (const AirportPtr psAirport)
+{
+	return true;
+}
+// results: If at least one runway is empty, return true; otherwise, return
+//          false
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+float getAverageLandingTime (const AirportPtr psAirport)
+{
+	return 0;
+}
+// results: Return the average landing waiting time
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+float getAverageTakeoffTime (const AirportPtr psAirport)
+{
+	return 0;
+}
+// results: Return the average takeoff waiting time
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+float getAverageFlyingTimeRemaining (const AirportPtr psAirport)
+{
+	return 0;
+}
+// results: Return the average flying time remaining
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+float getNumEmergencyLandings (const AirportPtr psAirport)
+{
+	return 0;
+}
+// results: Return the number of emergency landings
+// 					error code priority: ERROR_INVALID_AIRPORT
+
+float getNumCrashes (const AirportPtr psAirport)
+{
+	return 0;
+}
+// results: Return the number of crashes
+// 					error code priority: ERROR_INVALID_AIRPORT
