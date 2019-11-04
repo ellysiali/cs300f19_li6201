a8703c1268a686d9a215fab6b473d0d7f272bf34
Ellysia Li <li6201@pacificu.edu>
2019-11-03 18:33:11 -0800

    Implemented TreeWordCount

--- TreeWordCount/src/wordCountDriver.c
+++ TreeWordCount/src/wordCountDriver.c
@@ -11,7 +11,47 @@
 #include <stdlib.h>
 #include "../include/tree.h"
 
-int main ()
+int main (int argc, char* argv[])
 {
-	return 0;
+	int countBuffer;
+	char szWord[WORD_MAX + 1];
+	FILE* pInFile = NULL;
+	TreeNodePtr psRoot;
+
+	// Check number of arguments
+
+	if (argc != 2)
+	{
+		printf ("Invalid number of arguments\n");
+		return EXIT_FAILURE;
+	}
+
+  // Check valid file
+
+	pInFile = fopen (argv[1], "r");
+
+	if (NULL == pInFile)
+	{
+		printf ("Could not open file\n");
+		return EXIT_FAILURE;
+	}
+
+	trLoadErrorMessages ();
+	trCreate (&psRoot);
+
+	while (!feof (pInFile))
+	{
+		fscanf (pInFile, "%s", szWord);
+		if (!trInsert (&psRoot, szWord, 1))
+		{
+			trFind (psRoot, szWord, &countBuffer);
+			trUpdate (psRoot, szWord, countBuffer + 1);
+		}
+	}
+
+	trPrintInOrder (psRoot);
+
+	trTerminate (&psRoot);
+
+	return EXIT_SUCCESS;
 }
