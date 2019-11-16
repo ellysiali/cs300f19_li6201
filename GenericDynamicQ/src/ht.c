ed3f66d966d99c35de2bdb5926069cf06e754f0e
Ellysia Li <li6201@pacificu.edu>
2019-11-15 20:46:15 -0800

    Adding skeleton project for Hash Table

---
+++ GenericDynamicQ/src/ht.c
@@ -0,0 +1,14 @@
+/**************************************************************************
+ File name:  ht.c
+ Author:     Ellysia Li
+ Date:		   Nov 15, 2019
+ Class:		   CS300
+ Assignment: Hash Tables
+ Purpose:    Implementation for a hash table structure
+ *************************************************************************/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../include/ht.h"
+#include "../../GenericDynamicList/include/list.h"
---
+++ HashTable/include/ht.h
@@ -0,0 +1,125 @@
+/**************************************************************************
+ File name:  ht.h
+ Author:     Ellysia Li
+ Date:		   Nov 15, 2019
+ Class:		   CS300
+ Assignment: Hash Tables
+ Purpose:    This file defines the constants, data structures, and
+             function prototypes for implementing a hash table
+ *************************************************************************/
+
+#ifndef HT_H_
+#define HT_H_
+
+#include <stdbool.h>
+#include "../../GenericDynamicList/include/list.h"
+
+//*************************************************************************
+// Constants
+//*************************************************************************
+#define MAX_ERROR_HT_CHARS 64
+
+enum {NO_HT_ERROR = 0,
+			ERROR_NO_HT_CREATE,
+			ERROR_NO_HT_TERMINATE,
+			ERROR_INVALID_HT,
+			ERROR_FULL_HT,
+			ERROR_NULL_HT_PTR,
+			ERROR_EMPTY_HT}; // If this error name changes, change statement below
+#define NUMBER_OF_HT_ERRORS ERROR_EMPTY_HT - NO_HT_ERROR + 1
+
+//*************************************************************************
+// Error Messages
+//*************************************************************************
+#define LOAD_HT_ERRORS strcpy(gszHTErrors[NO_HT_ERROR], "No Error.");\
+strcpy(gszHTErrors[ERROR_NO_HT_CREATE], "Error: No Hash Table Create.");\
+strcpy(gszHTErrors[ERROR_NO_HT_TERMINATE], "Error: No Hash Table Terminate.");\
+strcpy(gszHTErrors[ERROR_INVALID_HT], "Error: Invalid Hash Table.");\
+strcpy(gszHTErrors[ERROR_FULL_HT], "Error: Full Hash Table.");\
+strcpy(gszHTErrors[ERROR_NULL_HT_PTR], "Error: NULL Pointer.");\
+strcpy(gszHTErrors[ERROR_EMPTY_HT], "Error: Empty Hash Table.");
+
+//*************************************************************************
+// User-defined types
+//*************************************************************************
+typedef struct HashTable *HashTablePtr;
+typedef struct HashTable
+{
+	ListPtr psTable;
+	int tableSize;
+//	void (*InsertFunction) (*void, *void);
+
+} HashTable;
+
+typedef struct HashTableElement
+{
+	void* pKey;
+	void* pData;
+} HashTableElement;
+
+//typedef void (*InsertFunction) (*void, *void);
+
+
+/**************************************************************************
+*										Allocation and Deallocation
+**************************************************************************/
+extern void htCreate (HashTablePtr psHTable);
+// results: If HT can be created, then HT exists and is empty
+//					otherwise, ERROR_NO_HT_CREATE
+
+extern void htTerminate (HashTablePtr psHTable);
+// results: If HT can be terminated, then HT no longer exists and is empty
+//				   otherwise, ERROR_NO_HT_TERMINATE
+
+extern void htLoadErrorMessages ();
+// results:	Loads the error message strings for the error handler to use
+//					No error conditions
+
+/**************************************************************************
+*									Checking the data in the hash table
+**************************************************************************/
+extern int htSize (const HashTablePtr psHTable);
+// results: Returns the number of buckets (size) of the hash table
+// 					error code priority: ERROR_INVALID_HT if HT is NULL
+
+extern bool htIsEmpty (const HashTablePtr psHTable);
+// results: If HT is empty, return true; otherwise, return false
+// 					error code priority: ERROR_INVALID_HT
+
+/**************************************************************************
+*									Inserting, retrieving,and updating values
+**************************************************************************/
+extern void htInsert (HashTablePtr psHTable, const void *pKey,
+																						 const void *pData);
+// requires:
+// results: Insert the data and key into the hash table.
+//					error code priority: ERROR_INVALID_HT, ERROR_NULL_HT_PTR
+
+extern void *htDelete (HashTablePtr psHTable, const void *pKey, void *pData);
+// requires: psHTable is not empty
+// results: Remove the data and key from the hash table, returning the data
+//					error code priority: ERROR_INVALID_HT, ERROR_NULL_HT_PTR,
+//															 ERROR_EMPTY_HT
+
+extern void *htUpdate (HashTablePtr psHTable, const void *pKey,
+		 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	 	const void *pData);
+// requires: psQueue is not empty
+// results: Updates the data from a given key in the hash table
+//					error code priority: ERROR_INVALID_HT, ERROR_NULL_HT_PTR,
+//															 ERROR_EMPTY_HT
+
+/**************************************************************************
+*													Peek and Print Operations
+**************************************************************************/
+extern void *htFind (HashTablePtr psHTable, const void *pKey, void *pData);
+// results: Find and return the data for a given key in the hash table
+// 						error code priority: ERROR_INVALID_HT, ERROR_NULL_HT_PTR,
+//																 ERROR_EMPTY_HT
+
+extern void *htPrint (HashTablePtr psHTable, void *pBuffer, int size);
+// requires: psQueue is not empty
+// results: Debugging purposes; print out each bucket and its chain;
+//					if the chain is empty, print out NULL
+// 						error code priority: ERROR_INVALID_Q, ERROR_EMPTY_HT
+
+#endif /* HT_H_ */
